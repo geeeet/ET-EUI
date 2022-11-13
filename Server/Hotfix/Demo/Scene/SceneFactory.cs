51a6111b11322ebf4db677f86fcb6063c4f20dba
RichangP <geet1044460266@gmail.com>
2022-11-13 11:22:02 +0800

    1.编写账号逻辑1

--- Server/Hotfix/Demo/Scene/SceneFactory.cs
+++ Server/Hotfix/Demo/Scene/SceneFactory.cs
@@ -11,7 +11,7 @@ namespace ET
             long instanceId = IdGenerater.Instance.GenerateInstanceId();
             return await Create(parent, instanceId, instanceId, parent.DomainZone(), name, sceneType);
         }
-        
+
         public static async ETTask<Scene> Create(Entity parent, long id, long instanceId, int zone, string name, SceneType sceneType, StartSceneConfig startSceneConfig = null)
         {
             await ETTask.CompletedTask;
@@ -35,6 +35,11 @@ namespace ET
                     break;
                 case SceneType.Location:
                     scene.AddComponent<LocationComponent>();
+                    break;
+                //ÕËºÅ·þÎñÆ÷
+                case SceneType.Account:
+                    scene.AddComponent<NetKcpComponent, IPEndPoint, int>(startSceneConfig.OuterIPPort, SessionStreamDispatcherType.SessionStreamDispatcherServerOuter);
+
                     break;
             }
 
--- Unity/Codes/Hotfix/Demo/Login/LoginHelper.cs
+++ Unity/Codes/Hotfix/Demo/Login/LoginHelper.cs
@@ -5,41 +5,72 @@ namespace ET
 {
     public static class LoginHelper
     {
-        public static async ETTask Login(Scene zoneScene, string address, string account, string password)
+        public static async ETTask<int> Login(Scene zoneScene, string address, string account, string password)
         {
+            A2C_LoginAccount loginAccount = null;
+            Session accountSession = null;
+
             try
             {
-                // 创建一个ETModel层的Session
-                R2C_Login r2CLogin;
-                Session session = null;
-                try
+                accountSession = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(address));
+                loginAccount = (A2C_LoginAccount)await accountSession.Call(new C2A_LoginAccount()
                 {
-                    session = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(address));
-                    {
-                        r2CLogin = (R2C_Login) await session.Call(new C2R_Login() { Account = account, Password = password });
-                    }
-                }
-                finally
-                {
-                    session?.Dispose();
-                }
-
-                // 创建一个gate Session,并且保存到SessionComponent中
-                Session gateSession = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(r2CLogin.Address));
-                gateSession.AddComponent<PingComponent>();
-                zoneScene.AddComponent<SessionComponent>().Session = gateSession;
-				
-                G2C_LoginGate g2CLoginGate = (G2C_LoginGate)await gateSession.Call(
-                    new C2G_LoginGate() { Key = r2CLogin.Key, GateId = r2CLogin.GateId});
-
-                Log.Debug("登陆gate成功!");
-
-                Game.EventSystem.PublishAsync(new EventType.LoginFinish() {ZoneScene = zoneScene}).Coroutine();
+                    AccountName = account,
+                    Password = password,
+                });
             }
             catch (Exception e)
             {
+                accountSession?.Dispose();
                 Log.Error(e);
+                return ErrorCode.Err_NetWorkError;
+                throw;
             }
-        } 
+
+            if (loginAccount.Error != ErrorCode.ERR_Success)
+            {
+                accountSession?.Dispose();
+                return loginAccount.Error;
+            }
+
+            //保存Session链接
+            zoneScene.AddComponent<SessionComponent>().Session = accountSession;
+
+            return ErrorCode.ERR_Success;
+
+            //try
+            //{
+            //    // 创建一个ETModel层的Session
+            //    R2C_Login r2CLogin;
+            //    Session session = null;
+            //    try
+            //    {
+            //        session = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(address));
+            //        {
+            //            r2CLogin = (R2C_Login) await session.Call(new C2R_Login() { Account = account, Password = password });
+            //        }
+            //    }
+            //    finally
+            //    {
+            //        session?.Dispose();
+            //    }
+
+            //    // 创建一个gate Session,并且保存到SessionComponent中
+            //    Session gateSession = zoneScene.GetComponent<NetKcpComponent>().Create(NetworkHelper.ToIPEndPoint(r2CLogin.Address));
+            //    gateSession.AddComponent<PingComponent>();
+            //    zoneScene.AddComponent<SessionComponent>().Session = gateSession;
+
+            //    G2C_LoginGate g2CLoginGate = (G2C_LoginGate)await gateSession.Call(
+            //        new C2G_LoginGate() { Key = r2CLogin.Key, GateId = r2CLogin.GateId});
+
+            //    Log.Debug("登陆gate成功!");
+
+            //    Game.EventSystem.PublishAsync(new EventType.LoginFinish() {ZoneScene = zoneScene}).Coroutine();
+            //}
+            //catch (Exception e)
+            //{
+            //    Log.Error(e);
+            //}
+        }
     }
 }
